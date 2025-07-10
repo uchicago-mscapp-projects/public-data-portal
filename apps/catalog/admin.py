d5d520d893d5693a8c4f147fb02b2371b9d56509
Maggie Larson <maggiel4rson@gmail.com>
2025-07-10 14:09:19 -0500

    registered models in admin.py files - dependent on catalog & ugc updates

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Publisher, DataSet, DataSetFile, IdentifierKind, Identifier, Crosswalk, TemporalCollection, CuratedCollection
 
 # Register your models here.
+
+admin.site.register(Publisher, DataSet, DataSetFile, IdentifierKind, Identifier, Crosswalk, TemporalCollection, CuratedCollection)
