e3e6c319ca5f26f48a8dc1978ba3788e271f5d1f
Maggie Larson <maggiel4rson@gmail.com>
2025-07-14 13:04:21 -0500

    remove comment

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import Publisher, DataSet, DataSetFile, IdentifierKind, Identifier, Crosswalk, TemporalCollection, CuratedCollection
 
-# Register your models here.
-
 admin.site.register(Publisher, DataSet, DataSetFile, IdentifierKind, Identifier, Crosswalk, TemporalCollection, CuratedCollection)
