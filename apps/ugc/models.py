af0d56f6a2cbfab7ab845c7d6a2979883d5f9478
Maggie Larson <maggiel4rson@gmail.com>
2025-07-14 16:41:01 -0500

    edits on ugc/models

--- apps/ugc/models.py
+++ apps/ugc/models.py
@@ -11,19 +11,19 @@ class Comment(models.Model):
 
     Note: We talked about pulling comments from all datasets in a temporal collection to show alongside that collection.
     """
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
-    dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="comments")
+    dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE, related_name="comments")
     text = models.TextField()
-    created_time = models.DateTimeField()
+    created_time = models.DateTimeField(auto_now_add=True)
     # like/popularity metric?
 
 class Project(models.Model):
     """
     An object created by a user to save datasets relevant to a particular project in one place.
     """
-    name = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_time = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    created_time = models.DateTimeField(auto_now_add=True)
 
 class ProjectDataSet(models.Model):
     """
@@ -34,5 +34,5 @@ class ProjectDataSet(models.Model):
     project = models.ForeignKey(Project, related_name="datasets")
     dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE, related_name="projects")
     # ordering ?
-    created_time = models.DateTimeField()
-    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    note = models.TextField()
