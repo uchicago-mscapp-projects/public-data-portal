16d6ec5789fdcb972358282ecb2baa1f2d88d7d7
jpt <dev@jpt.sh>
2025-07-30 20:41:33 -0500

    Adds an example / index view.

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from apps.catalog import views as cat_views
 
 urlpatterns = [
     path("djadmin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    # maps the default "/" URL to our index page
+    path("", cat_views.index),
 ]
 
 if settings.DEBUG and not settings.IS_TESTING:
