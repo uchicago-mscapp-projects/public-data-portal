983f912e9954dc45077476ac1e51bbdda35b8077
teddykolios <tkolios@wustl.edu>
2025-08-10 11:33:44 -0500

    added draft implementation for clear_dir

--- apps/catalog/management/commands/ingest.py
+++ apps/catalog/management/commands/ingest.py
@@ -1,5 +1,7 @@
 import importlib
 import structlog
+import os
+import shutil
 from django_typer.management import Typer
 from ingestion.utils import logger
 
@@ -23,3 +25,20 @@ def command(self, name: str):
         details = get_dataset_details(pd)
         logger.info("details", detail=details)
         # TODO: this should save the datasets to disk & then import them
+
+def empty_dir(name: str):
+    cwd = os.getcwd()
+    dir_path = f"{os.path.dirname(cwd)}/ingest_json/{name}"
+
+    for filename in os.listdir(dir_path):
+        filepath = os.path.join(dir_path, filename)
+        try:
+            if os.path.isfile(filepath):
+                os.unlink(filepath)
+            elif os.path.isdir(filepath):
+                shutil.rmtree(filepath)
+        except Exception as e:
+            logger.info(f"Failed to delete file {filepath}", detail=e)
+
+def ingest_to_db():
+    pass
