dc9df606d4fe07bde3143de18a7a1ce9efe27d63
teddykolios <tkolios@wustl.edu>
2025-08-11 10:25:24 -0500

    draft implementation for ingest_to_db and clear_dir functions, still contains some placeholder code

--- apps/catalog/management/commands/ingest.py
+++ apps/catalog/management/commands/ingest.py
@@ -2,8 +2,10 @@ import importlib
 import structlog
 import os
 import shutil
+import json
 from django_typer.management import Typer
 from ingestion.utils import logger
+from catalog.models import DataSet, Publisher, Region
 
 app = Typer()
 
@@ -26,6 +28,7 @@ def command(self, name: str):
         logger.info("details", detail=details)
         # TODO: this should save the datasets to disk & then import them
 
+
 def empty_dir(name: str):
     cwd = os.getcwd()
     dir_path = f"{os.path.dirname(cwd)}/ingest_json/{name}"
@@ -40,5 +43,78 @@ def empty_dir(name: str):
         except Exception as e:
             logger.info(f"Failed to delete file {filepath}", detail=e)
 
-def ingest_to_db():
-    pass
+
+def ingest_to_db(name: str):
+    db_entries = DataSet.objects.filter(publisher=name) # will need to assure this name is consistent in db?
+    db_entries_ids = set(db_entries.values_list("upstream_id", flat=True))
+
+    incoming_datasets = load_incoming_ds(name)
+    incoming_ds_ids = set()
+
+    if Publisher.objects.filter(name=name).exists(): # publisher object exists, retrieve from db
+        publisher = Publisher.objects.filter(name=name)
+    else: # publisher not in system yet, create entry
+        publisher = Publisher.objects.create(
+            name = dataset["publisher_name"],
+            kind = "gn", # will need to revisit this, figure out how we will set
+            url = dataset["publisher_url"]
+        )
+
+    for dataset in incoming_datasets:
+
+        if Region.objects.filter(country_code=dataset["region_country_code"]).exists(): # region object exists, retrieve from db
+            region = Region.objects.filter(country_code=dataset["region_country_code"])
+        else: # region not in system yet, create entry
+            region = Region.objects.create(
+                name = dataset["region_name"],
+                country_code = dataset["region_country_code"]
+            )
+
+        if dataset["upstream_id"] in db_entries_ids: # existing dataset entry, update
+            # only apply change to entry with matching publisher name And upstream_id for unique id
+            DataSet.objects.filter(publisher__name=dataset["publisher_name"], upstream_id=dataset["upstream_id"]).update(
+                name = dataset["name"],
+                description = dataset["description"],
+                upstream_upload_time = dataset["upstream_upload_time"],
+                start_date = dataset["start_date"],
+                end_date = dataset["end_date"],
+                publisher = publisher,
+                region = region,
+                source_url = dataset["source_url"],
+                upstream_id = dataset["upstream_id"],
+                license = dataset["license"],
+                temporal_collection = None,
+                curated_collection = None,
+                quality_score = 0
+            )
+        else: # no existing entry, create
+            DataSet.objects.create(
+                name = dataset["name"],
+                description = dataset["description"],
+                upstream_upload_time = dataset["upstream_upload_time"],
+                start_date = dataset["start_date"],
+                end_date = dataset["end_date"],
+                publisher = publisher,
+                region = region,
+                source_url = dataset["source_url"],
+                upstream_id = dataset["upstream_id"],
+                license = dataset["license"],
+                temporal_collection = None,
+                curated_collection = None,
+                quality_score = 0
+            )
+
+        incoming_ds_ids.add(dataset["upstream_id"])
+    
+    print("(Would be) Deleted record upstream_ids:")
+    for id in list(db_entries_ids - incoming_ds_ids):
+        print(id)
+
+
+def load_incoming_ds(name: str):
+    cwd = os.getcwd()
+    file_path = f"{os.path.dirname(cwd)}/ingest_json/{name}/json_file_name.json"
+
+    with open(file_path, "r") as f:
+        incoming_json = json.load(f)
+        return incoming_json["datasets"]
