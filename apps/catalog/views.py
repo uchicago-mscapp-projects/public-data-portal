70597f1d16a43735b6a2356a8a6b4f1cfd66320f
Maggie Larson <maggiel4rson@gmail.com>
2025-08-13 23:23:43 -0500

    initial notes for dataset detail view

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -35,3 +35,54 @@ def index(request):
     # most views will end with a call to render, passing the template name
     # and context dictionary
     return render(request, "index.html", context)
+
+
+
+def dataset_detail(request, dataset_id):
+
+    # TO DO: handle case when dataset id not found
+    #ds = DataSet.objects.get(id=dataset_id)
+
+    context = {
+        # LATER: get variables from dataset
+        """
+        "dataset_id": dataset_id,
+        "name": ds.name,
+        "description": ds.description,
+        "publisher": ds.publisher,
+        "region": ds.region,
+        "date_created": ds.created_at,
+        "start_date": ds.start_date,
+        "end_date": ds.end_date,
+        "dataset_id": dataset_id,
+        """
+        # FOR NOW: strings for testing
+        "dataset_id": dataset_id,
+        "name": "name",
+        "description": "description",
+        "publisher": "publisher",
+        "region": "region",
+        "date_created": "created_at",
+        "start_date": "start_date",
+        "end_date": "end_date",
+    }
+
+    # just to demonstrate, here is how you would obtain a parameter from the
+    # querystring (e.g. /?name=Bart). request.GET is a dictionary of these
+    # parameters
+
+    # TO DO: implement tabs to show collections / UGC with query strings
+    #context["name"] = request.GET.get("name", "anonymous user")
+
+    # Aside: Any time you see code taking untrusted user input be suspicious!
+    #  In older web frameworks, the code here would lead to a vulnerability
+    #  since a user could put ?name=<some malicious html...> and inject that
+    #  into the page.
+    #
+    # In practice, Django templates by default disallow HTML, which will
+    # prevent this attack. Keep in mind that all input from the internet
+    # is potentially hostile and should be escaped/treated with caution.
+
+    # most views will end with a call to render, passing the template name
+    # and context dictionary
+    return render(request, "dataset_detail.html", context)
