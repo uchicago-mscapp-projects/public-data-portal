8bc2f98e2ecc72a304604c6d43da51cfbcf22732
clleone <clleone@uchicago>
2025-08-21 08:03:50 -0500

    added directory deletion to clearcary function

--- apps/catalog/management/commands/ingest.py
+++ apps/catalog/management/commands/ingest.py
@@ -11,11 +11,11 @@ from apps.catalog.models import DataSet, Publisher, PublisherKind, Region
 
 app = Typer()
 
-@app.command()
 
+@app.command()
 def command(self, name: str, clearcary: bool, ingestonly: bool):
     if clearcary:
-        #not sure if this should be "us/cary_nc" or "us.cary_nc"
+        # not sure if this should be "us/cary_nc" or "us.cary_nc"
         if name != "us.cary_nc":
             raise ValueError("clearcary flag is to only be used with us/cary_nc scraper")
         clear_carync()
@@ -42,26 +42,32 @@ def command(self, name: str, clearcary: bool, ingestonly: bool):
                 continue
             save_to_json(details, name)
 
-    #ingest_to_db(name)
+    # ingest_to_db(name)
+
 
 def clear_carync():
-    '''resets carync for development testing'''
-    #empty out json
-    empty_dir("carync")
+    """resets cary_nc for development testing"""
+    # empty out directory, deletes all the json
+    empty_dir("us.cary_nc")
+    # delete the leftover, empty directory
+    dir_path = set_dir_path("us.cary_nc")
+    os.rmdir(dir_path)
+    logger.info("Existing Cary NC json and directory have been deleted.")
 
-    #clear carync datasets from db
+    # clear carync datasets from db
     p = Publisher.objects.get(name="Town of Cary")
     ## actual code to run
     # DataSet.objects.filter(publisher=p).delete() #delete datasets
     # p.delete() #delete publisher
-    # logger.info("Cary NC scraper data has been reset.")
+    # logger.info("Cary NC scraper has been removed from the database.")
 
-    #shows ids of datasets that would be deleted
+    # shows ids of datasets that would be deleted
     cary_dsets = DataSet.objects.filter(publisher=p)
     to_delete_ids = set(cary_dsets.values_list("upstream_id", flat=True))
     print("Cary NC datasets to be removed from database:")
     for id in list(to_delete_ids):
         print(id)
+
 
 def set_dir_path(name: str):
     cwd = os.getcwd()
@@ -69,14 +75,15 @@ def set_dir_path(name: str):
 
     return dir_path
 
+
 def prep_dir(name: str):
     dir_path = set_dir_path(name)
 
-    #if directory already exists, empty for overwrite
+    # if directory already exists, empty for overwrite
     if os.path.exists(dir_path):
         empty_dir(name)
         logger.info(f"Existing {name} directory has been emptied.")
-    #otherwise create directory for first scrape data
+    # otherwise create directory for first scrape data
     else:
         os.makedirs(dir_path, exist_ok=True)
         logger.info(f"New directory {name} has been created.")
@@ -114,10 +121,8 @@ def ingest_to_db(name: str):
     incoming_ds_ids = set()
 
     for i, dataset in enumerate(incoming_datasets):
-
         # only run on first iteration
         if i == 0:
-
             # load in existing db entries for publisher into flattened set
             db_entries = DataSet.objects.filter(publisher=dataset["publisher_name"])
             db_entries_ids = set(db_entries.values_list("upstream_id", flat=True))
@@ -126,17 +131,14 @@ def ingest_to_db(name: str):
             publisher, _ = Publisher.objects.get_or_create(
                 name=dataset["publisher_name"],
                 defaults={
-                    "kind": PublisherKind.GOV_NATIONAL, # will need to revisit this, figure out how we will set
-                    "url": dataset["publisher_url"]
-                }
+                    "kind": PublisherKind.GOV_NATIONAL,  # will need to revisit this, figure out how we will set
+                    "url": dataset["publisher_url"],
+                },
             )
 
         # for each iter, retrieve/create corresponding region obj for dataset in db
         region, _ = Region.objects.get_or_create(
-            country_code=dataset["region_country_code"],
-            defaults={
-                "name": dataset["region_name"]
-            }
+            country_code=dataset["region_country_code"], defaults={"name": dataset["region_name"]}
         )
 
         ds_values = {
@@ -150,13 +152,13 @@ def ingest_to_db(name: str):
             "source_url": dataset["source_url"],
             "upstream_id": dataset["upstream_id"],
             "license": dataset["license"],
-            "quality_score": 0
+            "quality_score": 0,
         }
 
         _, _ = DataSet.objects.update_or_create(
             publisher__name=dataset["publisher_name"],
             upstream_id=dataset["upstream_id"],
-            defaults=ds_values
+            defaults=ds_values,
         )
 
         incoming_ds_ids.add(dataset["upstream_id"])
@@ -177,4 +179,3 @@ def load_incoming_ds(name: str):
             incoming_datasets.append(incoming_json)
 
     return incoming_datasets
-
